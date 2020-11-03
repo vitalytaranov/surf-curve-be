b3e23e58faa36cbe7849eb9f04869cc38544dbe3
Vitaly Taranov <vtarnv@gmail.com>
2020-11-03 23:12:28 +0300

    update mock

--- product-service/src/db/products.ts
+++ product-service/src/db/products.ts
@@ -1,30 +1,44 @@
 export default [
   {
     id: 1,
-    title: 'surf board',
+    title: 'Lost Uber Driver XL surfboard 6ft 2 FCS II - White',
     price: 1000,
     description: 'surf board description',
     count: 5,
   },
   {
     id: 2,
-    title: 'winter surf board',
+    title: 'Lib Tech x Lost Hydra surfboard 5ft 11 - Red',
     price: 1100,
     description: 'winter surf board description',
     count: 3,
   },
   {
     id: 3,
-    title: 'summer surf board',
+    title: 'Firewire Helium Dominator 2.0 surfboard 6ft 2 FCS II - White',
     price: 999,
     description: 'summer surf board description',
     count: 1,
   },
   {
     id: 4,
-    title: 'fall surf board',
+    title: 'Lost Pro-Formance Driver 2.0 surfboard 6ft 0 FCS II - White',
     price: 400,
     description: 'fall surf board description',
+    count: 2,
+  },
+  {
+    id: 5,
+    title: 'Lost Cobra Killer surfboard 5ft 7 Futures - Tan',
+    price: 1000,
+    description: 'surf board description',
+    count: 2,
+  },
+  {
+    id: 6,
+    title: 'Channel Islands Mid surfboard 7ft 2 FCS II - Turquoise Resin Tint',
+    price: 300,
+    description: 'surf board description',
     count: 2,
   },
 ];
