7bed2bd875cd79ad2865f6187acfb799862575e3
Vitaly Taranov <vtarnv@gmail.com>
2020-11-03 22:52:13 +0300

    update mock

--- product-service/src/db/products.ts
+++ product-service/src/db/products.ts
@@ -1,17 +1,30 @@
 export default [
   {
     id: 1,
-    name: 'surf board',
+    title: 'surf board',
     price: 1000,
+    description: 'surf board description',
+    count: 5,
   },
   {
     id: 2,
-    name: 'winter surf board',
+    title: 'winter surf board',
     price: 1100,
+    description: 'winter surf board description',
+    count: 3,
   },
   {
     id: 3,
-    name: 'summer surf board',
+    title: 'summer surf board',
     price: 999,
+    description: 'summer surf board description',
+    count: 1,
+  },
+  {
+    id: 4,
+    title: 'fall surf board',
+    price: 400,
+    description: 'fall surf board description',
+    count: 2,
   },
 ];
