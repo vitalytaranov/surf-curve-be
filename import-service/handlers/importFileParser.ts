5c8aa49d80c39301433f24a72eacc8ed11833517
Vitaly Taranov <vtarnv@gmail.com>
2020-11-16 11:57:18 +0300

    add task 5

--- import-service/handlers/importFileParser.ts
+++ import-service/handlers/importFileParser.ts
@@ -1,7 +1,7 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import { S3 } from 'aws-sdk';
-import * as csv  from 'csv-parser';
+import * as csv from 'csv-parser';
 
 
 const { BUCKET } = process.env
@@ -12,36 +12,38 @@ export const importFileParser: APIGatewayProxyHandler = async (event, _context) 
 
   try {
     const s3 = new S3({ region: 'eu-west-1' });
+    console.log('event.Records: ', event.Records);
 
-    for (const record of event.Records) {
+    event.Records.forEach((record) => {
       console.log('record.s3.object: ', record.s3.object);
-      const readStream = s3.getObject({
+      const s3ReadStream = s3.getObject({
         Bucket: BUCKET,
         Key: record.s3.object.key,
       }).createReadStream();
 
-      const result = [];
-      readStream
+      s3ReadStream
         .pipe(csv())
-        .on('data', (data) => result.push(data))
+        .on('data', (chunk) => { console.log('parsed chunk: ', chunk) })
         .on('end', async () => {
+          const copyFrom = `${ BUCKET }/${ record.s3.object.key }`;
+          const copyTo = record.s3.object.key.replace('uploaded', 'parsed');
 
           await s3.copyObject({
             Bucket: BUCKET,
-            CopySource: `${ BUCKET }/${ record.s3.object.key }`,
-            Key: record.s3.object.key.replace('uploaded', 'parsed'),
-          });
+            CopySource: copyFrom,
+            Key: copyTo,
+          }).promise();
+          console.log(`copied from [${ copyFrom }] to [${ copyTo }]`);
 
           await s3.deleteObject({
             Bucket: BUCKET,
             Key: record.s3.object.key,
           }).promise();
-
-          console.log(`File ${ record.s3.object.key.split('/')[1] } has been parsed`);
+          console.log(`deleted from [${ copyFrom }]`);
         });
-    }
+    });
 
   } catch (error) {
-    console.log('ERROR: ', error);
+    console.log('error: ', error);
   }
 }
