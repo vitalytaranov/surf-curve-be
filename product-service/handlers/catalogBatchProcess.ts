e132dfbb12b4a3d0c151da11f9a1a6ad1dfa44dc
Vitaly Taranov <vtarnv@gmail.com>
2020-11-24 21:20:08 +0300

    add filter policy

--- product-service/handlers/catalogBatchProcess.ts
+++ product-service/handlers/catalogBatchProcess.ts
@@ -1,31 +1,10 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
-import { SNS } from 'aws-sdk';
 
 import { addProductToDb } from '../db/product';
-
-const { SNS_TOPIC_ARN } = process.env;
-
-function sendNotification(message: string, isError: boolean) {
-  console.log('@sendNotification');
-
-  const sns = new SNS({ region: 'eu-west-1' });
-  return sns.publish({
-    Subject: isError ? 'New product was not added' : 'New product was added',
-    Message: message,
-    TopicArn: SNS_TOPIC_ARN,
-    MessageAttributes: {
-      isError: {
-        DataType: 'String',
-        StringValue: isError ? 'true' : 'false',
-      },
-    },
-  }).promise();
-}
+import { sendNotification } from '../services/notification';
 
 export const catalogBatchProcess: APIGatewayProxyHandler = async (event) => {
-  console.log('@catalogBatchProcess: ', event);
-
   try {
     await Promise.all(
       event.Records.map(async (product) => {
@@ -35,7 +14,7 @@ export const catalogBatchProcess: APIGatewayProxyHandler = async (event) => {
           await sendNotification(product.body, false);
           console.log(`product has been processed`);
         } catch (error) {
-          console.log(error);
+          console.log(`product has not been processed`);
           await sendNotification(product.body, true);
         }
       }));
